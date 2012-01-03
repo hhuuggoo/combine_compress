8a5f8b564845573842ff5b1e3cca305c8ba285fd
Hugo <hugoshi@blob.(none)>
2012-01-02 21:03:19 -0500

    finisehd first pass

--- combine_compress.py
+++ combine_compress.py
@@ -2,6 +2,9 @@ import subprocess
 import simplejson
 import sys
 import shutil
+import os
+import os.path
+import re
 
 
 def minfile(out_name, in_name, mincommand):
@@ -21,18 +24,53 @@ def cat(output, inputs):
             with open(fname, 'rb') as infile:
                 shutil.copyfileobj(infile, outfile)
 
+def replace_html_file(key, replacement, html):
+    start_key = "<!-- start %s -->" % key
+    end_key = "<!-- end %s -->" % key
+    regexstring = "%s(\s*)([\w\W]*?)%s" % (start_key, end_key)
+    regobj = re.compile(regexstring)
+    replacement = "%s\g<1>%s\g<1>%s" % (start_key, replacement, end_key)
+    replacement = regobj.sub(replacement, html)
+    return replacement
+
+def replace_html_dir(key, newfile, path, css_or_js):
+    if css_or_js == 'css':
+        replacement = '<link rel="stylesheet" href="%s" type="text/css" >'
+    else:
+        replacement = '<script src="%s" type="text/javascript"></script>'
+    replacement = replacement % newfile
+    for dirpath, dirname, filenames in os.walk(path):
+        for fname in filenames:
+            fname = os.path.join(dirpath, fname)
+            with open(fname) as f:
+                html = f.read()
+                html = replace_html_file(key, replacement, html)
+            with open(fname, "w") as f:
+                f.write(html)
+        
 
 def combine_confobjs(confobjs):
     for confobj in confobjs:
         cat(confobj['output'], confobj['inputs'])
         
-def combine_compress(confobjs):
+def combine_compress_confobjs(confobjs):
     for confobj in confobjs:
         cat(confobj['output'], confobj['inputs'])
         minfile(confobj['output'], confobj['output'], confobj['compressor'])
+
+def html_replace_confobjs(confobjs):
+    for confobj in confobjs:
+        for htmldir in confobj['html_dirs']:
+            replace_html_dir(confobj['name'], confobj['output_link'],
+                             htmldir, confobj['type'])
     
 if __name__ == "__main__":
     conffile = sys.argv[1]
-    confobj = simplejson.load(conffile)
+    with open(conffile) as f:
+        data = f.read()
+        print data
+        confobjs = simplejson.loads(data)
+    combine_compress_confobjs(confobjs)
+    html_replace_confobjs (confobjs)
     
     
--- test/combine_compress_test.py
+++ test/combine_compress_test.py
@@ -2,6 +2,9 @@ import unittest
 import simplejson
 import tempfile
 import combine_compress
+import os
+import os.path
+import shutil
 
 class CombineCompressTestCase(unittest.TestCase):
     def test_file_combine(self):
@@ -72,8 +75,50 @@ class CombineCompressTestCaseFromConfig(unittest.TestCase):
         config = self.config
         config['inputs'] = [x.name for x in files]
         config['output'] = output_file.name
-        combine_compress.combine_compress([config])
+        combine_compress.combine_compress_confobjs([config])
         output_file.seek(0)
         data = output_file.read()
         assert data == 'function a0(a){return a}function a1(a){return a}function a2(a){return a}function a3(a){return a}function a4(a){return a}function a5(a){return a}function a6(a){return a}function a7(a){return a}function a8(a){return a}function a9(a){return a};'
+
+    def test_replace_html(self):
+        dirname = tempfile.mkdtemp()
+        with open(os.path.join(dirname, 'first.html'), 'w+') as f:
+            f.write(
+                """
+                <html>
+                </body>
+                <!-- start test -->
+                <link rel="stylesheet" href="/static/css/base.css" type="text/css" >
+                <link rel="stylesheet" href="/static/css/skeleton.css" type="text/css" >
+                <link rel="stylesheet" href="/static/css/layout.css" type="text/css" >
+                <!-- end test -->
+                <link rel="stylesheet" href="/static/css/outline.css" type="text/css" >
+                <link rel="stylesheet" href="/static/css/nav.css" type="text/css" >
+                </body>
+                </html>
+                """)
+        with open(os.path.join(dirname, 'second.html'), 'w+') as f:
+            f.write(
+                """
+                <html>
+                </body>
+                <!-- start test -->
+                <link rel="stylesheet" href="/static/css/base.css" type="text/css" >
+                <link rel="stylesheet" href="/static/css/skeleton.css" type="text/css" >
+                <link rel="stylesheet" href="/static/css/layout.css" type="text/css" >
+                <!-- end test -->
+                <link rel="stylesheet" href="/static/css/outline.css" type="text/css" >
+                <link rel="stylesheet" href="/static/css/nav.css" type="text/css" >
+                </body>
+                </html>
+                """)
+        self.config['html_dirs'] = [dirname]
+        self.config['output'] = os.path.join(dirname, 'base.css')
+        self.config['output_link'] = os.path.join('{{site_media}}/base.css')
+        combine_compress.replace_html_dir('test',
+                                              self.config['output_link'],
+                                              self.config['html_dirs'][0],
+                                              'css')
+        shutil.rmtree(dirname)
+        
 
